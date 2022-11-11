3cd027e6c57f364cb223641f716f25e597cc4c32
Gustavo Del Coli <gustavodelcoli@hotmail.com>
2022-11-11 17:38:52 -0300

    teste de commit no eclipse

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -8,5 +8,6 @@ public class PrimeiraClasseJava {
 		/*Escreve texto no console*/
 		System.out.println("Hello World");
 		System.out.println("My first program in Java");
+		System.out.println("Testing commit on github");
 	}
 }
