e8b2028b587d51d52ceae1caf3d57b585fee2723
Gustavo Del Coli <gustavodelcoli@hotmail.com>
2022-11-16 10:37:56 -0300

    adicionando o campo de idade na classe pessoa

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -13,6 +13,16 @@ public class PrimeiraClasseJava {
 		private String nome;
 		
 		private String sobrenome;
+		
+		private int idade;
+				
+		public int getIdade() {
+			return idade;
+		}
+
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
 
 		public String getNome() {
 			return nome;
