38ca08b0d22429dff942cbd44a6e6bd27fa68a90
Gustavo Del Coli <gustavodelcoli@hotmail.com>
2022-11-16 10:34:49 -0300

    alteracao e criacao da classe pessoa

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -9,8 +9,26 @@ public class PrimeiraClasseJava {
 		System.out.println("Hello World");
 		System.out.println("My first program in Java");
 		System.out.println("Testing commit on github");
-		
-		
-		
 	}
+		private String nome;
+		
+		private String sobrenome;
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getSobrenome() {
+			return sobrenome;
+		}
+
+		public void setSobrenome(String sobrenome) {
+			this.sobrenome = sobrenome;
+		}
+		
+		
 }
