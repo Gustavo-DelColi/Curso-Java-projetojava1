c14f6661c714d8be50fefd6864f4dfe279a3509d
Gustavo Del Coli <gustavodelcoli@hotmail.com>
2022-12-01 14:43:40 -0300

    9.10 - Os metódos comuns Getters e Setters

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -10,13 +10,32 @@ public class PrimeiraClasseJava {
 		/* new Aluno() é uma instancia (Criação de Obejto)*/
 		/*aluno1 é uma referencia para o objeto aluno*/
 		Aluno aluno1 = new Aluno(); /*Aqui será o João*/
-		aluno1.nome = "João";
-		aluno1.idade = 50;
+		aluno1.setNome("João da Silva");
+		aluno1.setIdade(30);
+		aluno1.setDataNascimento("02/01/1992");
+		aluno1.setRegistroGeral("10.945.052-9");
+		aluno1.setNumeroCpf("062.181.009-60");
+		aluno1.setNomeMae("Maria");
+		aluno1.setNomePai("Francisco");
+		aluno1.setDataMatricula("01/12/2022");
+		aluno1.setSerieMatriculado("9");
+		aluno1.setNomeEscola("Escola JDev Treinamento");
 		
-		System.out.println("Nome do aluno 1 é = " + aluno1.nome);
-		System.out.println("A idade é = " + aluno1.idade);
+		System.out.println("Nome é = " + aluno1.getNome());
+		System.out.println("Idade é = " + aluno1.getIdade());
+		System.out.println("Nascimento é = " + aluno1.getDataNascimento());
+
+	/*===============================================================================================*/
+		System.out.println("------------------------------------------------------");
 		
 		Aluno aluno2 = new Aluno(); /*Aqui será o Pedro*/
+		aluno2.setNome("Pedro");
+		aluno2.setIdade(40);
+		aluno2.setDataNascimento("10/10/1970");
+		
+		System.out.println("Aluno 2 Nome é = " + aluno2.getNome());
+		System.out.println("Aluno 2 Idade é = " + aluno2.getIdade());
+		System.out.println("Aluno 2 Nascimento é = " + aluno2.getDataNascimento());
 		
 		Aluno aluno3 = new Aluno(); /*Aqui será o Alex*/
 		
