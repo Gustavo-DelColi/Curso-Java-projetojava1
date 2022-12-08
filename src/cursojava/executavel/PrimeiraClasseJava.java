7e6a1a86d4d2d1c5abd52ca68ed48baa1f9d4758
Gustavo Del Coli <gustavodelcoli@hotmail.com>
2022-12-08 14:15:37 -0300

    9.22 - Criando a lista dinâmica das disciplinas do aluno

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -23,7 +23,7 @@ public class PrimeiraClasseJava {
 		String cpf = JOptionPane.showInputDialog("Qual o CPF?");
 		String mae = JOptionPane.showInputDialog("Qual o nome da mãe?");
 		String pai = JOptionPane.showInputDialog("Qual o nome do pai?");
-		String matricula = JOptionPane.showInputDialog("Qual o nome da matricula?");
+		String matricula = JOptionPane.showInputDialog("Qual a data da matricula?");
 		String serie = JOptionPane.showInputDialog("Qual a série?");
 		String escola = JOptionPane.showInputDialog("Qual o nome da escola?");
 		
@@ -41,31 +41,17 @@ public class PrimeiraClasseJava {
 		aluno1.setSerieMatriculado(serie);
 		aluno1.setNomeEscola(escola);
 		
-		Disciplina disciplina1 = new Disciplina();
-		disciplina1.setDisciplina("Banco de Dados");
-		disciplina1.setNota(90);
-		
-		aluno1.getDisciplinas().add(disciplina1);
-		
-		Disciplina disciplina2 = new Disciplina();
-		disciplina2.setDisciplina("matematica");
-		disciplina2.setNota(80);
-		
-		aluno1.getDisciplinas().add(disciplina2);
-		
-		
-		Disciplina disciplina3 = new Disciplina();
-		disciplina3.setDisciplina("geografia");
-		disciplina3.setNota(97);
-		
-		aluno1.getDisciplinas().add(disciplina3);
-		
-	
-		Disciplina disciplina4 = new Disciplina();
-		disciplina4.setDisciplina("Java Web");
-		disciplina4.setNota(70);
-		
-		aluno1.getDisciplinas().add(disciplina4);
+		for (int pos = 1; pos <= 4; pos++) {
+			String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina "+pos+" ?");
+			String notaDisciplina = JOptionPane.showInputDialog("Nota de disciplina "+pos+" ?");
+			
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));
+			
+			aluno1.getDisciplinas().add(disciplina);
+		}
+
 		
 		System.out.println(aluno1); /*Descrição do objeto na memória*/
 		System.out.println("Média do aluno = " + aluno1.getMediaNota());
