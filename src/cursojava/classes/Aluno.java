2bcd4ee51966849efb87863a7484513657efddca
Gustavo Del Coli <gustavodelcoli@hotmail.com>
2022-12-08 17:35:57 -0300

    9.31 - Classes e atributos estáticos

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import cursojava.constantes.StatusAluno;
+
 /* Está é nossa classe/objeto que representa o Aluno*/
 public class Aluno {
 
@@ -160,12 +162,12 @@ public class Aluno {
 		double media = this.getMediaNota();
 		if (media >= 50) {
 			if (media >= 70) {
-				return "Aluno está aprovado";
+				return StatusAluno.APROVADO;
 			} else {
-				return "Aluno em recuperação";
+				return StatusAluno.RECUPERACAO;
 			}
 		} else {
-			return "Aluno está reprovado";
+			return StatusAluno.REPROVADO;
 		}
 	}
 
